7f8c4c46aa7d45d26869efab211a0321eeb48c9d
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-04-17 12:25:11 -0300

    [ ADD ] favorito-1 todos os elementos

--- client/src/routes/pages/components/Favorito/favorito.tsx
+++ client/src/routes/pages/components/Favorito/favorito.tsx
@@ -1,5 +1,43 @@
 export default function Favorito1() {
     return (
-        <h1>1</h1>
+        <div className="favoritos-wrapper">
+          <div className="produto-content">
+              <div className="img-info">
+                  <div className="img">
+                      <div className="produto-imagem">
+                          <img src="/IMG/img-produtos/produto1.svg" alt=""/>
+                      </div>
+                  </div>
+                  <div className="infos">
+                      <div className="produto-nome">
+                          <p>Óculos Realidade Virtual VR Oculus Meta Quest 2 256gb</p>
+                      </div>
+                      <div className="promos">
+                          <div className="stars">
+                              <img src="/IMG/img-produtos/Stars.svg" alt=""/>
+                          </div>
+                          <div className="produto-preco">
+                              <p>R$ 3.799,99</p>
+                          </div>
+                              <div className="descontos">
+                                  <p>FRETE <br/>
+                                     GRATIS</p>
+                              </div>
+                      </div>
+                  </div>
+                  <div className="comprar">
+                      <div className="favoritos-favoritos">
+                          <img src="/IMG/heart copy.svg" alt="" id="P1"/>
+                      </div>
+                      <div className="btnComprar-favoritos">
+                          <button type="submit">
+                               <a href="#"> <img src="/IMG/img-produtos/Carrinho.svg" alt=""/> </a>
+                          </button>
+                      </div>
+                  </div>
+            </div>
+            <hr/>  
+            </div>
+        </div>    
     )
 }
