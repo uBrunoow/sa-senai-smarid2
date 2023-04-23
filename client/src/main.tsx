b7a50b507e872e4320da3e26e20d1c2513bf619a
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-04-23 15:15:08 -0300

    .

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
         element: <App />,
     },
     {
-        path: "Initialpage",
+        path: "initialpage",
         element: <Initialpage />,
     },
 
---
+++ client/src/routes/pages/components/Produtos2/produto-venda.tsx
@@ -0,0 +1,16 @@
+
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+
+// Import Swiper styles
+
+import "./styles.css";
+
+// import required modules
+
+export default function Swiper1() {
+  return (
+        
+  );
+}
