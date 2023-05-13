3426d19e087252b7ef33efc5950f6faaa0d306a5
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-05-12 22:09:01 -0300

    [ add ] hover nos produtos

--- client/src/routes/pages/components/Produtos/Swiper-2/swiper-2.tsx
+++ client/src/routes/pages/components/Produtos/Swiper-2/swiper-2.tsx
@@ -11,7 +11,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import "./styles.css";
+
 
 // import required modules
 import { Autoplay, Pagination } from "swiper";
--- client/src/routes/pages/components/Produtos2/produtos2.tsx
+++ client/src/routes/pages/components/Produtos2/produtos2.tsx
@@ -1,47 +1,54 @@
-import Stars from '/src/assets/IMG/img-produtos/Stars.svg'
-import Produtos from '/src/assets/IMG/img-produtos/produto1.svg'
-import { MdShoppingCart } from "react-icons/md"
+import Stars from "/src/assets/IMG/img-produtos/Stars.svg";
+import Produtos from "/src/assets/IMG/img-produtos/produto1.svg";
+import { MdShoppingCart } from "react-icons/md";
 
 // SASS É O MESMO ARQUIVO DO PRODUTOS 1
 
-
 export default function Produtos2() {
-    return(
-        <div className="Primeiro_produto">
-        <div className="Promos">
-            <div className="Stars_produto">
-                <img src={Stars} alt=""/>
+    return (
+        <a href={'/compra'} className="Primeiro_produto">
+            <div className="Promos">
+                <div className="Stars_produto">
+                    <img src={Stars} alt="" />
+                </div>
+                <div className="Produtos_restantes">
+                    <p>
+                        {" "}
+                        Restam <br />
+                        <span>30</span> <br />
+                        unid.
+                    </p>
+                </div>
+                <div className="Descontos">
+                    <p>
+                        30% <br />
+                        OFF
+                    </p>
+                </div>
             </div>
-            <div className="Produtos_restantes">
-                <p> Restam <br/>
-                    <span>30</span> <br/>
-                    unid.</p>
+            <div className="Imagem_do_produto">
+                <div className="produto-imagem">
+                    <img src={Produtos} alt="" />
+                </div>
             </div>
-            <div className="Descontos">
-                <p>30% <br/>
-                    OFF</p>
+            <div className="InformacoesProduto">
+                <div className="Produto_nome">
+                    <p>Óculos Realidade Virtual VR Oculus Meta Quest 2 256gb</p>
+                </div>
+                <div className="Preco_produto">
+                    <p>R$ 3.799,99</p>
+                </div>
             </div>
-        </div>
-        <div className="Imagem_do_produto">
-            <div className="produto-imagem">
-                <img src={Produtos} alt=""/>
+            <div className="Botao_comprar">
+                <div className="btnComprar">
+                    <button type="submit" className="BtnFlexComprar">
+                        <a href="#">
+                            {" "}
+                            <MdShoppingCart className="Cart_Shopping_produto" />{" "}
+                        </a>
+                    </button>
+                </div>
             </div>
-        </div>
-        <div className="InformacoesProduto">
-            <div className="Produto_nome">
-                <p>Óculos Realidade Virtual VR Oculus Meta Quest 2 256gb</p>
-            </div>
-            <div className="Preco_produto">
-                <p>R$ 3.799,99</p>
-            </div>
-        </div>
-        <div className="Botao_comprar">
-            <div className="btnComprar">
-                <button type="submit" className='BtnFlexComprar'>
-                      <a href="#"> <MdShoppingCart className='Cart_Shopping_produto'/> </a>
-                </button>
-            </div>
-        </div>
-      </div>
-    )
+        </a>
+    );
 }
