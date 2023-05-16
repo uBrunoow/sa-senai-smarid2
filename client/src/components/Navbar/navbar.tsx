6b5283c09da59e1109a7e55e6e407558a8580807
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-05-16 09:30:01 -0300

    [ add ] sidebar

--- client/src/components/Navbar/navbar.tsx
+++ client/src/components/Navbar/navbar.tsx
@@ -1,44 +1,100 @@
-import '../Navbar/navbar.sass'
-import '../Navbar/style.css'
+import "../Navbar/navbar.sass";
+import "../Navbar/style.css";
 
-import Sun from '../Navbar/img/sun.svg'
+import Sun from "../Navbar/img/sun.svg";
+import { AiOutlineClose } from "react-icons/ai";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillPersonFill } from "react-icons/bs"
+import { IoBasketSharp } from "react-icons/io5"
+import { AiFillHeart } from "react-icons/ai" 
+import { MdDiscount } from "react-icons/md"
+import { IoFlash } from "react-icons/io5"
+import { IoIosChatboxes } from "react-icons/io"
+import { AiFillLike } from "react-icons/ai"
+import { AiOutlineAim } from "react-icons/ai"
 
-export default function Navbar () {
+export default function Navbar() {
+    function AbrirSidebar() {
+        var sidebar = document.getElementById("sidebar") as HTMLDivElement;
+
+        if (sidebar.style.display == "none") {
+            sidebar.style.display = "block";
+        } else {
+            sidebar.style.display = "block";
+        }
+    }
+
+    function Close_sidebar() {
+        var sidebar = document.getElementById("sidebar") as HTMLDivElement;
+        sidebar.style.display = "none";
+    }
     return (
-        <nav>
-            <header>
-                <div className="container">
-                    <input type="checkbox" id="checkbox-menu"/>
-                    <label htmlFor="checkbox-menu">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </label> 
-                </div>
-                <div className="logo">Logo</div>
-                <div className="navbar">
-                    <a href={`/`}>Home</a>
-                    <a href={`/initialpage`}>Products</a>
-                    <a href="">Games</a>
-                    <a href="">About</a>
-                </div>
-                <div className="sign-in-up">
-                    <div className="sign-in">
-                        <button type="submit">
-                            <a href={`/login`}>Sign-in</a>
-                        </button>
+        <div>
+            <nav>
+                <header>
+                    <div className="container">
+                        <input type="checkbox" id="checkbox-menu" />
+                        <label htmlFor="checkbox-menu" onClick={AbrirSidebar}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </label>
                     </div>
-                    <div className="sign-up">
-                        <button type="submit">
-                            <a href={`/login`}>Sign-up</a>
-                        </button>
+                    <div className="logo">Logo</div>
+                    <div className="navbar">
+                        <a href={`/`}>Home</a>
+                        <a href={`/initialpage`}>Products</a>
+                        <a href="">Games</a>
+                        <a href="">About</a>
+                    </div>
+                    <div className="sign-in-up">
+                        <div className="sign-in">
+                            <button type="submit">
+                                <a href={`/login`}>Sign-in</a>
+                            </button>
+                        </div>
+                        <div className="sign-up">
+                            <button type="submit">
+                                <a href={`/login`}>Sign-up</a>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="light-dark">
+                        <img src={Sun} />
+                    </div>
+                </header>
+            </nav>
+            <main className="sidebar" id="sidebar">
+                <div className="flex-conta">
+                    <div className="conta_sidebar">
+                        <img src="https://i1.sndcdn.com/artworks-bEMhExX1BoD6F5Tu-atmOiw-t500x500.jpg" />
+                        <h1>Olá user</h1>
+                    </div>
+                    <div className="fechar-sidebar" id="Close_sidebar">
+                        <AiOutlineClose
+                            className="Icon_close"
+                            onClick={Close_sidebar}
+                        />
                     </div>
                 </div>
-            
-                <div className="light-dark">
-                <img src={Sun}/>
+                <div className="Links-sidebar">
+                    <ul>
+                        <li> <AiFillHome className="Link_icon"/> Minha conta</li>
+                        <li> <BsFillPersonFill className="Link_icon"/>Meus dados</li>
+                        <li> <IoBasketSharp className="Link_icon"/>Meus pedidos</li>
+                        <li> <AiFillHeart className="Link_icon"/>Favoritos</li>
+                        <li> <MdDiscount className="Link_icon"/> Ofertas do dia</li>
+                        <li> <IoFlash className="Link_icon"/> Acabaram de chegar</li>
+                        <li> <IoIosChatboxes className="Link_icon"/> Protocolos de atendimento</li>
+                        <li> <AiFillLike className="Link_icon"/> Avaliações</li>
+                        <li> <AiOutlineAim className="Link_icon"/>Mais Procurados</li>
+                    </ul>
+                    <div className="logout">
+                        <button>Logout</button>
+                    </div>
                 </div>
-            </header>
-        </nav>
-    )
+            </main>
+        </div>
+    );
 }
