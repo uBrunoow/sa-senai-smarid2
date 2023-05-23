3871f9fc4082f8bea6e533977be31ed25864a6db
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-05-23 07:48:35 -0300

    [add] initial config for server

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+
+const app = fastify()
+
+app.register(multipart)
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })
